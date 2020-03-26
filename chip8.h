fbd2997a322c3ba41304b2d583786329c88e4b81
Levi Jacoby <levi.jacoby1991@gmail.com>
2020-03-26 12:29:52 -0500

    turned registers into array

--- chip8.h
+++ chip8.h
@@ -1,41 +1,33 @@
 #ifndef __chip8_h
 #define __chip8_h
+#include<string>
 class chip8{
 	private:
 		unsigned short opcode;
-		//IR is a 16bit Reg that stores memory address
-		unsigned short IR;
 		//Memory of Chip
 		unsigned char memory[4096];
 		//stack
 		unsigned short stack[16];
+		//stack pointer
+		unsigned short stack_ptr;
+		//IR is a 16bit Reg that stores memory address
+		unsigned short IR;
 		//prgram counter
 		unsigned short PC;
 		//registers
-		unsigned char V0;
-		unsigned char V1;
-		unsigned char V2;
-		unsigned char V3;
-		unsigned char V4;
-		unsigned char V5;
-		unsigned char V6;
-		unsigned char V7;
-		unsigned char V8;
-		unsigned char V9;
-		unsigned char VA;
-		unsigned char VB;
-		unsigned char VC;
-		unsigned char VD;
-		unsigned char VE;
-		//special register for certain opcodes
-		unsigned char VF;
+		unsigned char V[16];
+		//graphics system
+		unsigned char gfx[64 * 32];
 		//timers for sound and delay
 		unsigned char sound_timer;
 		unsigned char delay_timer;
+		//key pad
+		unsigned char key[16];
 	
 	public:
 		void initialize();
 		void emulateCycle();
+		void loadGame(std::string);
 		bool drawFlag;
 };
 #endif
